3573298416f4375272ea038d6b9d5634918a7054
Macaron1 <43721994+Macaron1@users.noreply.github.com>
2023-12-05 20:32:56 +0500

    update

--- js/dark.js
+++ js/dark.js
@@ -19,23 +19,23 @@ const dark  = '\
 
 const light = '\
 	body {\
-		background:#fff !important;\
-		color:#212529 !important;\
-		background-image: url(/img/Mfv3HIcty.png), url(/img/white-scratone.png) !important;\
-		background-position: left bottom, left top !important;\
-		background-repeat: no-repeat, repeat !important;\
-		background-size: 200px, 123px !important;}\
-	a{color:#007BFF !important;}\
+		background:#fff;\
+		color:#212529;\
+		background-image: url(/img/Mfv3HIcty.png), url(/img/white-scratone.png);\
+		background-position: left bottom, left top;\
+		background-repeat: no-repeat, repeat;\
+		background-size: 200px, 123px;}\
+	a{color:#007BFF;}\
 	*::selection{background:#ff0064;}\
 	#mcrn-offcanvas {background-color:#fff;}\
 	#mcrn-offcanvas .mcrn-menu,#mcrn-offcanvas .mcrn-bio{background-color: #ffffffa8}\
-	#mcrn-offcanvas .mcrn-menu ul li a {color: #000 !important;}\
-	.mcrn-social li a {color: #000 !important;}\
-	#mcrn-offcanvas .mcrn-close-offcanvas{color: #cccccc !important;}\
+	#mcrn-offcanvas .mcrn-menu ul li a {color: #000;}\
+	.mcrn-social li a {color: #000;}\
+	#mcrn-offcanvas .mcrn-close-offcanvas{color: #cccccc;}\
 	.mcrn-nav-toggle i::before, .mcrn-nav-toggle i::after{background: #535659;}\
 	.mcrn-nav-toggle i{background: #535659;}\
-	.mcrn-post-entry article h2 a {color: #000 !important;}\
-	#results{background-color: #ffffffa8 !important;}\
+	.mcrn-post-entry article h2 a {color: #000;}\
+	#results{background-color: #ffffffa8;}\
 	:root {--scrollbarBgColor: rgba(0, 0, 0, .05);}\
 	*::-webkit-scrollbar-thumb {background-image: linear-gradient(180deg,var(--scrollbarThumbColor) 0%,var(--scrollbarThumbColorSecond) 90%);--scrollbarThumbColor: rgba(0, 0, 0, .15);}'
 
@@ -48,6 +48,7 @@ if (localStorage.getItem('theme') == 'dark') {
 }
 
 document.getElementById('themeBtn').addEventListener( "click" , function() {
+	removeGlobalStyle();
     if (localStorage.getItem('theme') == 'dark') {
         addGlobalStyle(light);
         localStorage.setItem('theme', 'light')
@@ -65,5 +66,13 @@ function addGlobalStyle(css) {
     style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
+	style.id = 'dark-style';
     head.appendChild(style);
+}
+
+function removeGlobalStyle() {
+    var styleElement = document.getElementById('dark-style');
+    if (styleElement) {
+        styleElement.parentNode.removeChild(styleElement);
+    }
 }
